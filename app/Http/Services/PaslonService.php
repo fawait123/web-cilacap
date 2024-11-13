8f0d1f80ad953a2afee7b1b52fb33932fc450d2c
Achmad Fawait <fawait626@gmail.com>
2024-11-13 17:04:01 +0700

    fix: count vote paslon

--- app/Http/Services/PaslonService.php
+++ app/Http/Services/PaslonService.php
@@ -20,11 +20,11 @@ class PaslonService
     {
         switch (request('view')) {
             case 'subdistrict':
-                return '(select COALESCE(sum(v.vote),0) from votes v where v.paslonID = paslons.id and v.districtID = "3") as total';
+                return '(select COALESCE(sum(v.vote),0) from votes v where v.paslonID = paslons.id) as total';
             case 'village':
-                return '(select COALESCE(sum(v.vote),0) from votes v where v.paslonID = paslons.id and v.subdistrictID = "' . request('filter.parent_id') . '") as total';
+                return '(select COALESCE(sum(v.vote),0) from votes v where v.paslonID = paslons.id and v.subdistrictID = "'.request('filter.parent_id').'") as total';
             case 'tps' || 'input-vote':
-                return '(select COALESCE(sum(v.vote),0) from votes v where v.paslonID = paslons.id and v.subdistrictID = "' . request('subdistrictID') . '" and v.villageID = "' . request('villageID') . '") as total';
+                return '(select COALESCE(sum(v.vote),0) from votes v where v.paslonID = paslons.id and v.subdistrictID = "'.request('subdistrictID').'" and v.villageID = "'.request('villageID').'") as total';
             default:
                 return '(select COALESCE(sum(v.vote),0) from votes v where v.paslonID = paslons.id) as total';
         }
@@ -34,11 +34,11 @@ class PaslonService
     {
         switch (request('view')) {
             case 'subdistrict':
-                return '(select COALESCE(sum(v.vote),0) from votes v where v.districtID = "3") as total_vote';
+                return '(select COALESCE(sum(v.vote),0) from votes v) as total_vote';
             case 'village':
-                return '(select COALESCE(sum(v.vote),0) from votes v where v.subdistrictID = "' . request('filter.parent_id') . '") as total_vote';
+                return '(select COALESCE(sum(v.vote),0) from votes v where v.subdistrictID = "'.request('filter.parent_id').'") as total_vote';
             case 'tps' || 'input-vote':
-                return '(select COALESCE(sum(v.vote),0) from votes v where v.subdistrictID = "' . request('subdistrictID') . '" and v.villageID = "' . request('villageID') . '") as total_vote';
+                return '(select COALESCE(sum(v.vote),0) from votes v where v.subdistrictID = "'.request('subdistrictID').'" and v.villageID = "'.request('villageID').'") as total_vote';
             default:
                 return '(select COALESCE(sum(v.vote),0) from votes v) as total_vote';
         }
